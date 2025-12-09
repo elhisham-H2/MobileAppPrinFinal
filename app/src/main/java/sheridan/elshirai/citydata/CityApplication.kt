fbfe6089abec5617acb9e0abd12a5938c9efbc93
Hisham El-Shiraida (H²) <hisham.el.shiraida@gmail.com>
2025-12-09 12:19:57 -0500

    Setup Navigation, Hilt App, and Main Activity

---
+++ app/src/main/java/sheridan/elshirai/citydata/CityApplication.kt
@@ -0,0 +1,7 @@
+package sheridan.elshirai.citydata
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CityApplication : Application()
--- app/src/main/java/sheridan/elshirai/citydata/MainActivity.kt
+++ app/src/main/java/sheridan/elshirai/citydata/MainActivity.kt
@@ -3,45 +3,39 @@ package sheridan.elshirai.citydata
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import sheridan.elshirai.citydata.ui.theme.CityDataTheme
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import sheridan.elshirai.citydata.ui.theme.CityListScreen
+import sheridan.elshirai.citydata.ui.theme.CityViewModel
+import sheridan.elshirai.citydata.ui.theme.CityDetailScreen
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            CityDataTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            val navController = rememberNavController()
+            val viewModel: CityViewModel = hiltViewModel()
+
+            NavHost(navController = navController, startDestination = "list") {
+                composable("list") {
+                    val list by viewModel.cityList.collectAsState()
+                    CityListScreen(cityList = list) { id ->
+                        viewModel.selectCity(id)
+                        navController.navigate("detail")
+                    }
+                }
+
+                composable("detail") {
+                    val city by viewModel.selectedCity.collectAsState()
+                    CityDetailScreen(city = city)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CityDataTheme {
-        Greeting("Android")
     }
 }
